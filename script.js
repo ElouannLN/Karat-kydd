91e8c5c0ee516fe469107a8176ea1562c9c6d8ea
miszczuk-cell <miszczuk.ivan@gmail.com>
2020-02-08 21:05:12 +0100

    Les cubes

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
-var titre = document.querySelector(".facebook");
-titre.addEventListener("click",function(e){
-  titre.style.fontSize ="300em";
-});
+for(var i = 0;i < 20;i++)
+{
+  document.querySelector(".groupe").appendChild(document.querySelector(".cube").cloneNode(true));
+}
+
+var lesCubes = document.querySelectorAll(".cube");
+for (var i = 0; i < lesCubes.length; i++)
+{
+  lesCubes[i].addEventListener("click",function(e){
+    e.currentTarget.style.transform = "translateZ(20em)";
+  });
+}
+
+function nombreAleatoire(min,max)
+{
+  var interval = max - min;
+  var aleatoire = (Math.floor(Math.random()*(interval+1)))+min;
+  return aleatoire;
+}
+
+function mouvementCube(){
+  setInterval(function(){
+    for (var i = 0; i < lesCubes.length; i++)
+    {
+      var em = nombreAleatoire(-20,10);
+      lesCubes[i].style.transform = "translateZ(" + em + "em)";
+    }
+  },3000);
+}
+
+mouvementCube();
