de633dac1bc34a2d5a3f4d06aaae2478978a1d25
miszczuk-cell <miszczuk.ivan@gmail.com>
2020-02-09 02:13:51 +0100

    i_çuçij

--- script.js
+++ script.js
@@ -1,31 +1,26 @@
-for(var i = 0;i < 20;i++)
-{
+for (var i = 0; i < 20; i++) {
   document.querySelector(".groupe").appendChild(document.querySelector(".cube").cloneNode(true));
 }
-
 var lesCubes = document.querySelectorAll(".cube");
-for (var i = 0; i < lesCubes.length; i++)
-{
-  lesCubes[i].addEventListener("click",function(e){
+for (var i = 0; i < lesCubes.length; i++) {
+  lesCubes[i].addEventListener("click", function(e) {
     e.currentTarget.style.transform = "translateZ(20em)";
   });
 }
 
-function nombreAleatoire(min,max)
-{
+function nombreAleatoire(min, max) {
   var interval = max - min;
-  var aleatoire = (Math.floor(Math.random()*(interval+1)))+min;
+  var aleatoire = (Math.floor(Math.random() * (interval + 1))) + min;
   return aleatoire;
 }
 
-function mouvementCube(){
-  setInterval(function(){
-    for (var i = 0; i < lesCubes.length; i++)
-    {
-      var em = nombreAleatoire(-20,10);
+function mouvementCube() {
+  setInterval(function() {
+    for (var i = 0; i < lesCubes.length; i++) {
+      var em = nombreAleatoire(-20, 10);
       lesCubes[i].style.transform = "translateZ(" + em + "em)";
     }
-  },3000);
+  }, 3000);
 }
 
 mouvementCube();
