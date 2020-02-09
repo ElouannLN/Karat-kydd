3804238b27d8f9c78c9e2beff4402a60be362bb4
ElouannLN <e.le-nezet@laposte.net>
2020-02-09 02:13:39 +0100

    Jsp

--- script.js
+++ script.js
@@ -1,36 +1,36 @@
-for(var i = 0;i < 64;i++)
-{
+  for(var i = 0;i < 64;i++)
+  {
   document.querySelector(".groupe").appendChild(document.querySelector(".cube").cloneNode(true));
-}
+  }
 
-var lesCubes = document.querySelectorAll(".cube");
-for (var i = 0; i < lesCubes.length; i++)
-{
+  var lesCubes = document.querySelectorAll(".cube");
+  for (var i = 0; i < lesCubes.length; i++)
+  {
   lesCubes[i].addEventListener("mouseover",function(e){
-    e.currentTarget.style.transform = "translateZ(20em) rotateX(500deg)";
-setTimeout(function () {
-  e.currentTarget.style.transform = "translateZ(0em) rotateX(0deg)"
-}, 10000);
-});
-}
+  e.currentTarget.style.transform = "translateZ(20em) rotateX(500deg)";
+  setTimeout(function () {
+    alert("fgdfg");
+  e.currentTarget.style.transform = "translateZ(0em) rotateX(0deg)"; }, 3000);
+  });
+  }
 
-function nombreAleatoire(min,max)
-{
+  function nombreAleatoire(min,max)
+  {
   var interval = max - min;
   var aleatoire = (Math.floor(Math.random()*(interval+1)))+min;
   return aleatoire;
-}
+  }
 
 
 
-/*function mouvementCube(){
+  /*function mouvementCube(){
   setInterval(function(){
-    for (var i = 0; i < lesCubes.length; i++)
-    {
-      var em = nombreAleatoire(-20,10);
-      lesCubes[i].style.transform = "translateZ(" + em + "em)";
-    }
+  for (var i = 0; i < lesCubes.length; i++)
+  {
+    var em = nombreAleatoire(-20,10);
+    lesCubes[i].style.transform = "translateZ(" + em + "em)";
+  }
   },3000);
-}
+  }
 
-mouvementCube();*/
+  mouvementCube();*/
